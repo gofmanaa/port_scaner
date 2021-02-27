599832a77656dd5beb4975160481e275ac915fe9
gofmanaa <gofmanaa@gmail.com>
2021-02-27 11:23:30 +0200

    fix issues

--- data/data.go
+++ data/data.go
@@ -1,6 +1,9 @@
 package data
 
+import "sync"
+
 type Set struct {
+	sync.Mutex
 	set map[interface{}]struct{}
 }
 
@@ -9,10 +12,14 @@ func NewSet() *Set {
 }
 
 func (s *Set) Add(data interface{}) {
+	s.Lock()
+	defer s.Unlock()
 	s.set[data] = struct{}{}
 }
 
 func (s *Set) Delete(data interface{}) {
+	s.Lock()
+	defer s.Unlock()
 	delete(s.set, data)
 }
 
--- main.go
+++ main.go
@@ -3,33 +3,50 @@ package main
 import (
 	"flag"
 	"fmt"
-	"github.com/gofmanaa/port_scanner/data"
 	"net"
 	"runtime"
 	"strconv"
 	"sync"
 	"time"
+
+	"github.com/gofmanaa/port_scanner/data"
 )
 
 var address string
 
-const ttl = 500
+const ttl = 2
 
 func init() {
 	flag.StringVar(&address, "addr", "localhost", "Host name")
 }
 
 func main() {
+	fmt.Println("Start sacanning...")
 	flag.Parse()
-	out := make(chan int, 100)
+	ch := make(chan int, 1<<16)
 
-	scanPorts(address, out)
-	getChanel(out)
+	cpus := runtime.NumCPU()
+	runtime.GOMAXPROCS(cpus)
+
+	fmt.Println("CpuNum: ", cpus)
+
+	scanPorts(address, ch)
+	openPortsMap := getChanel(ch)
+	openPorts := openPortsMap.GetInt()
+
+	if len(openPorts) > 0 {
+		for _, openPort := range openPorts {
+			fmt.Printf("Port %d [open]\n", openPort)
+		}
+	} else {
+		fmt.Printf("Host %s have no open ports.\n", address)
+	}
+	fmt.Println("Done.")
 }
 
 func isOpenPort(address string, port int) bool {
 	address = address + ":" + strconv.Itoa(port)
-	con, err := net.DialTimeout("tcp", address, time.Microsecond*ttl)
+	con, err := net.DialTimeout("tcp", address, time.Second*ttl)
 	if err != nil {
 		return false
 	}
@@ -45,8 +62,6 @@ func scanPorts(address string, out chan int) {
 	wg.Add(1)
 	go startScan(address, &wg, out)
 
-	gorutines := runtime.NumGoroutine()
-	fmt.Println("gorut: ", gorutines)
 	wg.Wait()
 }
 
@@ -65,9 +80,7 @@ func getChanel(in <-chan int) *data.Set {
 
 	for openPort := range in {
 		res.Add(openPort)
-		fmt.Printf("Port %d open\n", openPort)
 	}
 
-	fmt.Println(*res)
 	return res
 }
